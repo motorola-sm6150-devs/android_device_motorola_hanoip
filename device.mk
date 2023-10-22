77605e9efea1924fa44061797877bf7ee56cc16d
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-22 18:29:52 +0530

    hanoip: Define screen resolution for boot animation
    Change-Id: I1e2147af79c1b1830849ea1f56657d5eca22de27

--- device.mk
+++ device.mk
@@ -11,6 +11,10 @@ TARGET_IS_VAB := true
 # Inherit from sm6150-common
 $(call inherit-product, device/motorola/sm6150-common/common.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2460
+TARGET_SCREEN_WIDTH := 1080
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

92ca230f11472777604b04010a1d72453d71b312
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-23 01:37:15 +0530

    hanoip: Define kernel config
    Change-Id: I453cd189a38c47f459050c2c90f58baeff2579a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,8 @@ TARGET_OTA_ASSERT_DEVICE := hanoip
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
+
 # Inherit from the proprietary version
 include vendor/motorola/hanoip/BoardConfigVendor.mk

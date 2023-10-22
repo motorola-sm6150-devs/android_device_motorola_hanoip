3deef651f9b7141d4ab1dffa7161db06af9695a0
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-23 01:37:01 +0530

    hanoip: Inherit from sm6150-common
    Change-Id: I83e6bb34d69d263c41e30e72fd112b02de714f79

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sm6150-common
+$(call inherit-product, device/motorola/sm6150-common/common.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
